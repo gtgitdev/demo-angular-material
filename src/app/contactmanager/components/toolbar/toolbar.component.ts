d46e7c1d11bd92522cdb82e97bde1f671e58e67e
gtgitdev <g.turton@hotmail.com>
2019-03-03 05:30:27 -0500

    added snackbar

--- src/app/contactmanager/components/toolbar/toolbar.component.ts
+++ src/app/contactmanager/components/toolbar/toolbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material';
 import { NewContactDialogComponent } from '../new-contact-dialog/new-contact-dialog.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -10,7 +11,9 @@ import { NewContactDialogComponent } from '../new-contact-dialog/new-contact-dia
 export class ToolbarComponent implements OnInit {
   @Output() toggleSidenav = new EventEmitter<void>();
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog,
+              private snackBar: MatSnackBar,
+              private router: Router) { }
 
   ngOnInit() {
   }
@@ -22,6 +25,19 @@ export class ToolbarComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe((result) => {
       console.log(result);
+      if (result) {
+        this.openSnackBar('Contact Added', 'Navigate')
+        .onAction().subscribe(() => {
+          this.router.navigate(['/contactmanager', result.id]);
+        });
+
+      }
+    });
+  }
+
+  openSnackBar(message: string, action: string): MatSnackBarRef<SimpleSnackBar> {
+    return this.snackBar.open(message, action, {
+      duration: 5000,
     });
   }
 }
